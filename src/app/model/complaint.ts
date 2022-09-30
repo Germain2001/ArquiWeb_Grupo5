1413ce04ccfcd7faba4c6a962fb7299fccd0fe14
Germain2001 <germain.08willy@gmail.com>
2022-09-30 13:43:54 -0500

    Versión Willy

---
+++ src/app/model/complaint.ts
@@ -0,0 +1,9 @@
+import { PoliceStation } from "./policestation";
+//import { Zone } from "./zone";
+
+export class Complaint {
+    id:number=0;
+    NComplaint:string="";
+    policeStation:PoliceStation = new PoliceStation();
+
+}
---
+++ src/app/model/policestation.ts
@@ -0,0 +1,8 @@
+import { Address } from "./address";
+
+export class PoliceStation {
+    id: number = 0;
+    address: Address = new Address();
+    NName: string = "";
+    TPhone: string = "";
+}
