6765341cd68c1a7953d0b669544df3443981a37a
SebastianLHF <locofx38@gmail.com>
2022-09-30 17:58:30 -0500

    Provincia completo

---
+++ src/app/app.module.ts
@@ -0,0 +1,124 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { DepartmentComponent } from './page/department/department.component';
+import { DeparmentListarComponent } from './page/department/deparment-listar/deparment-listar.component';
+import { DepartmentEditarComponent } from './page/department/department-editar/department-editar.component';
+import { DepartmentBuscarComponent } from './page/department/department-buscar/department-buscar.component';
+import { DepartmentInsertComponent } from './page/department/department-insert/department-insert.component';
+import { DepartmentDialogoComponent } from './page/department/deparment-listar/department-dialogo/department-dialogo.component';
+
+import { ProvinceComponent } from './page/province/province.component';
+import { ProvinceListarComponent } from './page/province/province-listar/province-listar.component';
+import { ProvinceInsertComponent } from './page/province/province-insert/province-insert.component';
+import { ProvinceDialogoComponent } from './page/province/province-listar/province-dialogo/province-dialogo.component';
+
+import { AddressComponent } from './page/address/address.component';
+import { AddressListarComponent } from './page/address/address-listar/address-listar.component';
+
+import { DistrictComponent } from './page/district/district.component';
+import { DistrictListarComponent } from './page/district/district-listar/district-listar.component';
+
+import { PersonComponent } from './page/person/person.component';
+import { PersonListarComponent } from './page/person/person-listar/person-listar.component';
+
+import { ComplaintComponent } from './page/complaint/complaint.component';
+import { ComplaintListarComponent } from './page/complaint/complaint-listar/complaint-listar.component';
+import { ComplaintInsertComponent } from './page/complaint/complaint-insert/complaint-insert.component';
+import { ComplaintBuscarComponent } from './page/complaint/complaint-buscar/complaint-buscar.component';
+
+import { UserComponent } from './page/user/user.component';
+import { UserListarComponent } from './page/user/user-listar/user-listar.component';
+
+import { PolicestationComponent } from './page/policestation/policestation.component';
+import { PolicestationListarComponent } from './page/policestation/policestation-listar/policestation-listar.component';
+import { PolicestationInsertComponent } from './page/policestation/policestation-insert/policestation-insert.component';
+import { PolicestationBuscarComponent } from './page/policestation/policestation-buscar/policestation-buscar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table'
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import{MatIconModule} from'@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RolComponent } from './page/rol/rol.component';
+
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { LoginComponent } from './page/login/login.component';
+import { MatSelectModule } from '@angular/material/select';
+import { ProvinceBuscarComponent } from './page/province/province-buscar/province-buscar.component';
+import { DistrictInsertComponent } from './page/district/district-insert/district-insert.component';
+import { DistrictBuscarComponent } from './page/district/district-buscar/district-buscar.component';
+import { DistrictDialogoComponent } from './page/district/district-listar/district-dialogo/district-dialogo.component';
+import { LandingPageComponent } from './page/landing-page/landing-page.component';
+import { NavbarlComponent } from './page/navbarl/navbarl.component';
+import { ComplaintDialogoComponent } from './page/complaint/complaint-listar/complaint-dialogo/complaint-dialogo.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentComponent,
+    DeparmentListarComponent,
+    ProvinceComponent,
+    ProvinceListarComponent,
+    AddressComponent,
+    AddressListarComponent,
+    DistrictComponent,
+    DistrictListarComponent,
+    PersonComponent,
+    PersonListarComponent,
+    ComplaintComponent,
+    ComplaintListarComponent,
+    PolicestationComponent,
+    PolicestationListarComponent,
+    UserComponent,
+    UserListarComponent,
+    DepartmentInsertComponent,
+    RolComponent,
+    DepartmentEditarComponent,
+    DepartmentBuscarComponent,
+    DepartmentDialogoComponent,
+    ProvinceInsertComponent,
+    LoginComponent,
+    ProvinceDialogoComponent,
+    ProvinceBuscarComponent,
+    DistrictInsertComponent,
+    DistrictBuscarComponent,
+    DistrictDialogoComponent,
+    LandingPageComponent,
+    NavbarlComponent,
+    PolicestationInsertComponent,
+    PolicestationBuscarComponent,
+    ComplaintDialogoComponent,
+    ComplaintBuscarComponent,
+    ComplaintInsertComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/page/landing-page/landing-page.component.ts
@@ -0,0 +1,16 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor(public route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+}
