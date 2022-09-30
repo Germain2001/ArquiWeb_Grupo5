aec68249460e407bee228f4d1fa5801d2a7ce689
mo2345283432 <108906315+mo2345283432@users.noreply.github.com>
2022-09-30 16:40:47 -0500

    Commit Mariana

---
+++ src/app/page/department/department-insert/department-insert.component.ts
@@ -0,0 +1,35 @@
+import { Router } from '@angular/router';
+import { Department } from 'src/app/model/department';
+import { DepartmentService } from 'src/app/service/department.service';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-department-insert',
+  templateUrl: './department-insert.component.html',
+  styleUrls: ['./department-insert.component.css']
+})
+export class DepartmentInsertComponent implements OnInit {
+
+  department: Department = new Department();
+  mensaje: string = "";
+
+  constructor(private departmentService: DepartmentService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  aceptar() {
+    if (this.department.NDepartment.length > 0 ) {
+      this.departmentService.insertar(this.department).subscribe(data => {
+        this.departmentService.listar().subscribe(data => {
+          this.departmentService.setLista(data);
+        })
+      })
+      this.router.navigate(['department']);
+    } else {
+      this.mensaje = "Complete los valores requeridos";
+    }
+  }
+
+}
